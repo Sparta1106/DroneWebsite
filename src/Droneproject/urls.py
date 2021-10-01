730459bdb4b7671dbedc162f911beaa47d94dab7
Ultraempoleon <danccoral@hotmail.com>
2021-09-30 22:12:45 -0700

    File rearrange
    Changes to Dronewebsite
    - Moved Dashboard.html & main.html to DroneProject/templates
    
    - Created app named Pages, pages will store all of the info of our main pages
        * views specifically
    
    - Dronproject/urls
        * changes the paths to include the moving of dashboard.html and main.html

--- src/Droneproject/urls.py
+++ src/Droneproject/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from pages.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='dashboard.html'),
     path('', include('accounts.urls'))
 ]
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
     path('products/', views.products),
     path('customer/', views.customer),
     
