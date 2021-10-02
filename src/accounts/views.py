38dbf8b94914749675278897b84961086ab74008
Ultraempoleon <danccoral@hotmail.com>
2021-10-01 23:51:40 -0700

    Deleted templates/accounts folder
    Deleted the accounts folder that had the html files and moves them into the folder above it
    before it was templates/accounts/html pages
    now its templates/html pages

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-# Calls(Render) the html file(template) located in templates/accounts/products.html
+# Calls(Render) the html file(template) located in templates/products.html
 def products(request):
-    return render(request, 'accounts/products.html')
+    return render(request, 'products.html')
 
-# Calls(Render) the html file(template) located in templates/accounts/customer.html
+# Calls(Render) the html file(template) located in templates/customer.html
 def customer(request):
-    return render(request, 'accounts/customer.html' )
+    return render(request, 'customer.html' )
 
